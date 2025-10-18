676f05fa92e3cac819d83ab4dbbc3a4113a59469
qwzx-qwas <1913840946@qq.com>
2025-10-18 17:37:42 +0800

    change release

---
+++ src/include/storage/page/b_plus_tree_leaf_page.h
@@ -0,0 +1,101 @@
+//===----------------------------------------------------------------------===//
+//
+//                         CMU-DB Project (15-445/645)
+//                         ***DO NO SHARE PUBLICLY***
+//
+// Identification: src/include/page/b_plus_tree_leaf_page.h
+//
+// Copyright (c) 2018-2024, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "storage/page/b_plus_tree_page.h"
+
+namespace bustub {
+
+#define B_PLUS_TREE_LEAF_PAGE_TYPE BPlusTreeLeafPage<KeyType, ValueType, KeyComparator>
+#define LEAF_PAGE_HEADER_SIZE 16
+#define LEAF_PAGE_SLOT_CNT ((BUSTUB_PAGE_SIZE - LEAF_PAGE_HEADER_SIZE) / (sizeof(KeyType) + sizeof(ValueType)))
+
+/**
+ * Store indexed key and record id (record id = page id combined with slot id,
+ * see `include/common/rid.h` for detailed implementation) together within leaf
+ * page. Only support unique key.
+ *
+ * Leaf page format (keys are stored in order):
+ *  ---------
+ * | HEADER |
+ *  ---------
+ *  ---------------------------------
+ * | KEY(1) | KEY(2) | ... | KEY(n) |
+ *  ---------------------------------
+ *  ---------------------------------
+ * | RID(1) | RID(2) | ... | RID(n) |
+ *  ---------------------------------
+ *
+ *  Header format (size in byte, 16 bytes in total):
+ *  -----------------------------------------------
+ * | PageType (4) | CurrentSize (4) | MaxSize (4) |
+ *  -----------------------------------------------
+ *  -----------------
+ * | NextPageId (4) |
+ *  -----------------
+ */
+INDEX_TEMPLATE_ARGUMENTS
+class BPlusTreeLeafPage : public BPlusTreePage {
+ public:
+  // Delete all constructor / destructor to ensure memory safety
+  BPlusTreeLeafPage() = delete;
+  BPlusTreeLeafPage(const BPlusTreeLeafPage &other) = delete;
+
+  /**
+   * After creating a new leaf page from buffer pool, must call initialize
+   * method to set default values
+   * @param max_size Max size of the leaf node
+   */
+  void Init(int max_size = LEAF_PAGE_SLOT_CNT);
+
+  // Helper methods
+  auto GetNextPageId() const -> page_id_t;
+  void SetNextPageId(page_id_t next_page_id);
+  auto KeyAt(int index) const -> KeyType;
+
+  /**
+   * @brief For test only return a string representing all keys in
+   * this leaf page formatted as "(key1,key2,key3,...)"
+   *
+   * @return The string representation of all keys in the current internal page
+   */
+  auto ToString() const -> std::string {
+    std::string kstr = "(";
+    bool first = true;
+
+    for (int i = 0; i < GetSize(); i++) {
+      KeyType key = KeyAt(i);
+      if (first) {
+        first = false;
+      } else {
+        kstr.append(",");
+      }
+
+      kstr.append(std::to_string(key.ToString()));
+    }
+    kstr.append(")");
+
+    return kstr;
+  }
+
+ private:
+  page_id_t next_page_id_;
+  // Array members for page data.
+  KeyType key_array_[LEAF_PAGE_SLOT_CNT];
+  ValueType rid_array_[LEAF_PAGE_SLOT_CNT];
+  // (Fall 2024) Feel free to add more fields and helper functions below if needed
+};
+
+}  // namespace bustub
---
+++ src/storage/index/b_plus_tree.cpp
@@ -0,0 +1,120 @@
+#include "storage/index/b_plus_tree.h"
+#include "storage/index/b_plus_tree_debug.h"
+
+namespace bustub {
+
+INDEX_TEMPLATE_ARGUMENTS
+BPLUSTREE_TYPE::BPlusTree(std::string name, page_id_t header_page_id, BufferPoolManager *buffer_pool_manager,
+                          const KeyComparator &comparator, int leaf_max_size, int internal_max_size)
+    : index_name_(std::move(name)),
+      bpm_(buffer_pool_manager),
+      comparator_(std::move(comparator)),
+      leaf_max_size_(leaf_max_size),
+      internal_max_size_(internal_max_size),
+      header_page_id_(header_page_id) {
+  WritePageGuard guard = bpm_->WritePage(header_page_id_);
+  auto root_page = guard.AsMut<BPlusTreeHeaderPage>();
+  root_page->root_page_id_ = INVALID_PAGE_ID;
+}
+
+/*
+ * Helper function to decide whether current b+tree is empty
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::IsEmpty() const -> bool { return true; }
+
+/*****************************************************************************
+ * SEARCH
+ *****************************************************************************/
+/*
+ * Return the only value that associated with input key
+ * This method is used for point query
+ * @return : true means key exists
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::GetValue(const KeyType &key, std::vector<ValueType> *result) -> bool {
+  // Declaration of context instance.
+  Context ctx;
+  (void)ctx;
+  return false;
+}
+
+/*****************************************************************************
+ * INSERTION
+ *****************************************************************************/
+/*
+ * Insert constant key & value pair into b+ tree
+ * if current tree is empty, start new tree, update root page id and insert
+ * entry, otherwise insert into leaf page.
+ * @return: since we only support unique key, if user try to insert duplicate
+ * keys return false, otherwise return true.
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::Insert(const KeyType &key, const ValueType &value) -> bool {
+  // Declaration of context instance.
+  Context ctx;
+  (void)ctx;
+  return false;
+}
+
+/*****************************************************************************
+ * REMOVE
+ *****************************************************************************/
+/*
+ * Delete key & value pair associated with input key
+ * If current tree is empty, return immediately.
+ * If not, User needs to first find the right leaf page as deletion target, then
+ * delete entry from leaf page. Remember to deal with redistribute or merge if
+ * necessary.
+ */
+INDEX_TEMPLATE_ARGUMENTS
+void BPLUSTREE_TYPE::Remove(const KeyType &key) {
+  // Declaration of context instance.
+  Context ctx;
+  (void)ctx;
+}
+
+/*****************************************************************************
+ * INDEX ITERATOR
+ *****************************************************************************/
+/*
+ * Input parameter is void, find the leftmost leaf page first, then construct
+ * index iterator
+ * @return : index iterator
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::Begin() -> INDEXITERATOR_TYPE { return INDEXITERATOR_TYPE(); }
+
+/*
+ * Input parameter is low key, find the leaf page that contains the input key
+ * first, then construct index iterator
+ * @return : index iterator
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::Begin(const KeyType &key) -> INDEXITERATOR_TYPE { return INDEXITERATOR_TYPE(); }
+
+/*
+ * Input parameter is void, construct an index iterator representing the end
+ * of the key/value pair in the leaf node
+ * @return : index iterator
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::End() -> INDEXITERATOR_TYPE { return INDEXITERATOR_TYPE(); }
+
+/**
+ * @return Page id of the root of this tree
+ */
+INDEX_TEMPLATE_ARGUMENTS
+auto BPLUSTREE_TYPE::GetRootPageId() -> page_id_t { return 0; }
+
+template class BPlusTree<GenericKey<4>, RID, GenericComparator<4>>;
+
+template class BPlusTree<GenericKey<8>, RID, GenericComparator<8>>;
+
+template class BPlusTree<GenericKey<16>, RID, GenericComparator<16>>;
+
+template class BPlusTree<GenericKey<32>, RID, GenericComparator<32>>;
+
+template class BPlusTree<GenericKey<64>, RID, GenericComparator<64>>;
+
+}  // namespace bustub
